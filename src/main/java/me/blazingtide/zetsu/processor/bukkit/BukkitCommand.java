68c56b73f43b10519f18752fa681e5df0ba2108b
BlazingTide <45052821+BlazingTide@users.noreply.github.com>
2022-01-10 12:55:38 -0800

    Disable all tab complete stuff (The library really needs to be rewritten)

--- src/main/java/me/blazingtide/zetsu/processor/bukkit/BukkitCommand.java
+++ src/main/java/me/blazingtide/zetsu/processor/bukkit/BukkitCommand.java
@@ -5,9 +5,6 @@ import me.blazingtide.zetsu.tabcomplete.TabCompleteHandler;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class BukkitCommand extends Command {
 
@@ -23,14 +20,5 @@ public class BukkitCommand extends Command {
     @Override
     public boolean execute(@NotNull CommandSender commandSender, @NotNull String s, @NotNull String[] strings) {
         return processor.onCommand(commandSender, this, s, strings);
-    }
-
-    @Override
-    @Nullable
-    public List<String> tabComplete(@NotNull CommandSender sender,
-                                    @NotNull String alias,
-                                    @NotNull String[] args) throws IllegalArgumentException {
-        //It's easier to separate chunks of code into different classes.
-        return handler.getListener().onTabComplete(sender, this, alias, args);
     }
 }
